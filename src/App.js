b43f2bd380a56f08bf6abf7c6df5a70f41fbb2b6
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-20 12:35:16 +0200

    add page container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-const App = () => (
-  <div className="App">
-    <h1>Birthday Reminder</h1>
-  </div>
-);
+import React, { useState } from 'react';
+import List from './components/List';
+import data from './components/Data';
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+
+  const handleClick = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>
+          {people.length}
+          {' '}
+          birthdays today
+        </h3>
+        <List people={people} />
+        <button type="button" onClick={handleClick}>
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
+};
 
 export default App;
